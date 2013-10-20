0161da1cc602d4b3d0e7e3027de6c03dbba38019
razon <shizonera@gmail.com>
2013-10-20 23:50:03 +0900

    Create project files from giter8 template

---
+++ src/main/scala/tengu/Tengu.scala
@@ -0,0 +1,26 @@
+package tengu
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Tengu extends Application {
+  @Override
+  def start(primaryStage: Stage) {
+    primaryStage.setTitle("Tengu")
+    primaryStage.setScene(new Scene(FXMLLoader.load(getClass.getResource("/Tengu.fxml"))))
+    primaryStage.show
+  }
+
+  def launch(args: Array[String]) {
+    Application.launch(args: _*)
+  }
+}
+
+object Tengu {
+  def main(args: Array[String]) {
+    new Tengu launch args
+  }
+}
+
---
+++ src/test/scala/tengu/TenguSpec.scala
@@ -0,0 +1,13 @@
+package tengu
+
+import org.scalatest.FunSpec
+
+class TenguSpec extends FunSpec {
+  describe("1 == 1") {
+    it("should equals 1"){
+      assert(1 == 1)
+    }
+  }
+}
+
+
